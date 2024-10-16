5104bd477c04006007cad36583b31cc4acac548f
MatiasMoller <136556009+MatiasMoller@users.noreply.github.com>
2024-10-16 13:04:19 +1100

    creating script

--- Assets/CustomEditorWindow/Editor/BasicObjectSpawner.cs
+++ Assets/CustomEditorWindow/Editor/BasicObjectSpawner.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class BasicObjectSpawner : MonoBehaviour
+public class BasicObjectSpawner : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    string objectBaseName = "";
+    int objectID = 1;
+    GameObject objectToSpawn;
+    float objectScale;
+    float spawnRadius = 5f;
+
+    [MenuItem("Tools/Basic Object Spawner")]
+    public static void ShowWindow()
     {
-        
+        GetWindow(typeof(BasicObjectSpawner));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        GUILayout.Label("Spawn New Object", EditorStyles.boldLabel);
+
+        objectBaseName = EditorGUILayout.TextField("Base Name", objectBaseName);
+        objectID = EditorGUILayout.IntField("Object ID", objectID);
+        objectScale = EditorGUILayout.Slider("Object Scale", objectScale, 0.5f, 3f);
+        spawnRadius = EditorGUILayout.FloatField("Spawn Radius", spawnRadius);
+        objectToSpawn = EditorGUILayout.ObjectField("Prefab to Spawn", objectToSpawn, typeof(GameObject), false) as GameObject;
+
+        if(GUILayout.Button("Spawn Object"))
+        {
+            SpawnObject();
+        }
+    }
+
+    private void SpawnObject()
+    {
+        if(objectToSpawn == null)
+        {
+            Debug.LogError("Error: Please assign an object");
+            return;
+        }
+        if (objectBaseName == string.Empty)
+        {
+            Debug.LogError("Error: Please assign an object");
+            return;
+        }
+
+        Vector2 spawnCircle = Random.insideUnitCircle * spawnRadius;
+        Vector3 spawnPos = new Vector3(spawnCircle.x, 0f, spawnCircle.y);
+
+        GameObject gameObject = Instantiate(objectToSpawn, spawnPos, Quaternion.identity);
+        gameObject.name = objectBaseName + objectID;
+        gameObject.transform.localScale = Vector3.one * objectScale;
+
+
+        objectID++;
     }
 }
